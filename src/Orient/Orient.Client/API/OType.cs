9c1bc404f21c169e621fdcb3b31f96b27187df10
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-08 22:57:28 +0100

    Introduce enum of available types in orient.

---
+++ src/Orient/Orient.Client/API/OType.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Orient.Client
+{
+    public enum OType
+    {
+        Boolean,
+        Byte,
+        Integer,
+        Short,
+        Long,
+        Float,
+        Double,
+        Decimal,
+        Date,
+        String,
+        Binary,
+        Embedded,
+        EmbeddedList,
+        EmbeddedSet,
+        EmbeddedMap,
+        Link,
+        LinkList,
+        LinkSet,
+        LinkMap
+    }
+}
