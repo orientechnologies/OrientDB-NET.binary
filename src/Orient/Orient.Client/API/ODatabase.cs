0b0366d1204921fe073f3ebe6296f8bf77784003
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-12 11:34:12 +0100

    Implement non idempotent command execution on database level.

--- src/Orient/Orient.Client/API/ODatabase.cs
+++ src/Orient/Orient.Client/API/ODatabase.cs
@@ -21,16 +21,40 @@ namespace Orient.Client
 
         public List<ORecord> Query(string sql)
         {
+            return Query(sql, "*:0");
+        }
+
+        public List<ORecord> Query(string sql, string fetchPlan)
+        {
             CommandPayload payload = new CommandPayload();
             payload.Type = CommandPayloadType.Sql;
             payload.Text = sql;
             payload.NonTextLimit = -1;
-            payload.FetchPlan = "*:0";
+            payload.FetchPlan = fetchPlan;
             payload.SerializedParams = new byte[] { 0 };
 
             Command operation = new Command();
             operation.OperationMode = OperationMode.Synchronous;
             operation.ClassType = CommandClassType.Idempotent;
+            operation.CommandPayload = payload;
+
+            DataObject dataObject = _connection.ExecuteOperation<Command>(operation);
+
+            return dataObject.Get<List<ORecord>>("Content");
+        }
+
+        public object Command(string sql)
+        {
+            CommandPayload payload = new CommandPayload();
+            payload.Type = CommandPayloadType.Sql;
+            payload.Text = sql;
+            payload.NonTextLimit = -1;
+            payload.FetchPlan = "";
+            payload.SerializedParams = new byte[] { 0 };
+
+            Command operation = new Command();
+            operation.OperationMode = OperationMode.Synchronous;
+            operation.ClassType = CommandClassType.NonIdempotent;
             operation.CommandPayload = payload;
 
             DataObject dataObject = _connection.ExecuteOperation<Command>(operation);
