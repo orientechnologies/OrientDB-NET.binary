e546e89b93b9916e47f5095453bed8eb38916345
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-13 17:13:58 +0100

    Rename property.

--- src/Orient/Orient.Client/API/OProperty.cs
+++ src/Orient/Orient.Client/API/OProperty.cs
@@ -5,7 +5,7 @@ namespace Orient.Client
     [AttributeUsage(AttributeTargets.Property)]
     public class OProperty : Attribute
     {
-        public string MapTo { get; set; }
+        public string MappedTo { get; set; }
         public OType Type { get; set; }
     }
 }
