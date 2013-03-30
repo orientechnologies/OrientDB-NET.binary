406fa4ec26e6542b24ab74896f89f86a8e94a6d5
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-31 00:14:12 +0100

    Refactor sql select.

--- src/Orient/Orient.Client/API/Query/OSqlSelect.cs
+++ src/Orient/Orient.Client/API/Query/OSqlSelect.cs
@@ -2,66 +2,43 @@
 using Orient.Client.Protocol;
 using Orient.Client.Protocol.Operations;
 
+// SELECT [FROM <Target> [LET <Assignment>*](<Projections>]) [<Condition>*](WHERE) [BY <Field>](GROUP) [BY <Fields>* [ASC|DESC](ORDER)*] [<SkipRecords>](SKIP) [<MaxRecords>](LIMIT)
+
 namespace Orient.Client
 {
     public class OSqlSelect
     {
         private Connection _connection;
-        private string _sql;
+        private SqlQuery _sqlQuery;
 
-        internal OSqlSelect(Connection connection)
+        internal OSqlSelect(Connection connection, string sql)
         {
             _connection = connection;
-            _sql = "";
-        }
-
-        // SELECT [FROM <Target> [LET <Assignment>*](<Projections>]) [<Condition>*](WHERE) [BY <Field>](GROUP) [BY <Fields>* [ASC|DESC](ORDER)*] [<SkipRecords>](SKIP) [<MaxRecords>](LIMIT)
-        public OSqlSelect Select(params string[] projections)
-        {
-            int iteration = 0;
-            _sql = string.Join(" ", Q.Select, "");
-
-            foreach (string projection in projections)
-            {
-                _sql += projection;
-
-                iteration++;
-
-                if (iteration < projections.Length)
-                {
-                    _sql += string.Join(" ", ",", "");
-                }
-            }
-
-            return this;
-        }
-
-        public OSqlSelect From(string target)
-        {
-            _sql += string.Join(" ", "", Q.From, target);
-
-            return this;
+            _sqlQuery = new SqlQuery(sql);
         }
 
         public OSqlSelect From<T>()
         {
-            _sql += string.Join(" ", "", Q.From, typeof(T).Name);
+            return From(typeof(T).Name);
+        }
+
+        public OSqlSelect From(string target)
+        {
+            _sqlQuery.Join("", Q.From, target);
 
             return this;
         }
 
-        public List<ORecord> ToList()
+        public List<ORecord> Run()
         {
-            OCommandResult commandResult = Execute(_sql, "*:0");
-
-            return commandResult.ToList();
+            return Run("*:0");
         }
 
-        private OCommandResult Execute(string sql, string fetchPlan)
+        public List<ORecord> Run(string fetchPlan)
         {
             CommandPayload payload = new CommandPayload();
             payload.Type = CommandPayloadType.Sql;
-            payload.Text = sql;
+            payload.Text = _sqlQuery.Value;
             payload.NonTextLimit = -1;
             payload.FetchPlan = fetchPlan;
             payload.SerializedParams = new byte[] { 0 };
@@ -71,7 +48,9 @@ namespace Orient.Client
             operation.ClassType = CommandClassType.Idempotent;
             operation.CommandPayload = payload;
 
-            return new OCommandResult(_connection.ExecuteOperation<Command>(operation));
+            OCommandResult commandResult = new OCommandResult(_connection.ExecuteOperation<Command>(operation));
+
+            return commandResult.ToList();
         }
     }
 }
