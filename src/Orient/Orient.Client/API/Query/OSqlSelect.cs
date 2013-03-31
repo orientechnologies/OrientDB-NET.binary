b0a4001624969d4739f4c6dddd6ba35754b7e03c
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-31 20:48:14 +0200

    Rename And to Also since it will be used in where statement and introduce more where related functionality.

--- src/Orient/Orient.Client/API/Query/OSqlSelect.cs
+++ src/Orient/Orient.Client/API/Query/OSqlSelect.cs
@@ -17,6 +17,8 @@ namespace Orient.Client
             _sqlQuery = new SqlQuery(sql);
         }
 
+        #region SELECT statement
+
         public OSqlSelect As(string alias)
         {
             _sqlQuery.Join("", Q.As, alias);
@@ -24,12 +26,16 @@ namespace Orient.Client
             return this;
         }
 
-        public OSqlSelect And(string projection)
+        public OSqlSelect Also(string projection)
         {
             _sqlQuery.Join(Q.Comma, projection);
 
             return this;
         }
+
+        #endregion
+
+        #region FROM statement
 
         public OSqlSelect From<T>()
         {
@@ -47,6 +53,91 @@ namespace Orient.Client
 
             return this;
         }
+
+        #endregion
+
+        #region WHERE statement
+
+        public OSqlSelect Where(string field)
+        {
+            _sqlQuery.Join("", Q.Where, field);
+
+            return this;
+        }
+
+        public OSqlSelect And(string field)
+        {
+            _sqlQuery.Join("", Q.And, field);
+
+            return this;
+        }
+
+        public OSqlSelect Or(string field)
+        {
+            _sqlQuery.Join("", Q.Or, field);
+
+            return this;
+        }
+
+        public OSqlSelect Equals<T>(T item)
+        {
+            _sqlQuery.Join("", Q.Equals, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect NotEquals<T>(T item)
+        {
+            _sqlQuery.Join("", Q.NotEquals, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect Lesser<T>(T item)
+        {
+            _sqlQuery.Join("", Q.Lesser, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect LesserEqual<T>(T item)
+        {
+            _sqlQuery.Join("", Q.LesserEqual, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect Greater<T>(T item)
+        {
+            _sqlQuery.Join("", Q.Greater, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect GreaterEqual<T>(T item)
+        {
+            _sqlQuery.Join("", Q.GreaterEqual, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect Like<T>(T item)
+        {
+            _sqlQuery.Join("", Q.Like, SqlQuery.ToString(item));
+
+            return this;
+        }
+
+        public OSqlSelect IsNull()
+        {
+            _sqlQuery.Join("", Q.Is, Q.Null);
+
+            return this;
+        }
+
+        #endregion
+
+        #region Run overloads
 
         public List<T> Run<T>() where T: class, new()
         {
@@ -70,7 +161,7 @@ namespace Orient.Client
         {
             CommandPayload payload = new CommandPayload();
             payload.Type = CommandPayloadType.Sql;
-            payload.Text = _sqlQuery.Value;
+            payload.Text = _sqlQuery.ToString();
             payload.NonTextLimit = -1;
             payload.FetchPlan = fetchPlan;
             payload.SerializedParams = new byte[] { 0 };
@@ -83,6 +174,13 @@ namespace Orient.Client
             OCommandResult commandResult = new OCommandResult(_connection.ExecuteOperation<Command>(operation));
 
             return commandResult.ToList();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return _sqlQuery.ToString();
         }
     }
 }
