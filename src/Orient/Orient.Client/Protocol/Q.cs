d335adc922b2062d21ff1d34a2ec2accb9c9c0b4
Tomas Bosak <bosak.tomas@gmail.com>
2013-03-31 20:46:19 +0200

    Add more query keywords.

--- src/Orient/Orient.Client/Protocol/Q.cs
+++ src/Orient/Orient.Client/Protocol/Q.cs
@@ -5,17 +5,31 @@ namespace Orient.Client.Protocol
     internal static class Q
     {
         internal static string _ = " ";
+        internal static string And = "AND";
         internal static string As = "AS";
+        internal static string Between = "BETWEEN";
         internal static string Class = "CLASS";
         internal static string Cluster = "CLUSTER";
         internal static string Comma = ",";
         internal static string Create = "CREATE";
         internal static string Edge = "EDGE";
+        internal static string Equals = "=";
         internal static string Extends = "EXTENDS";
         internal static string From = "FROM";
+        internal static string Greater = ">";
+        internal static string GreaterEqual = ">=";
+        internal static string In = "IN";
+        internal static string Is = "IS";
+        internal static string Lesser = "<";
+        internal static string LesserEqual = "<=";
+        internal static string Like = "LIKE";
+        internal static string NotEquals = "!=";
+        internal static string Null = "NULL";
+        internal static string Or = "OR";
         internal static string Select = "SELECT";
         internal static string Set = "SET";
         internal static string To = "TO";
         internal static string Vertex = "VERTEX";
+        internal static string Where = "WHERE";
     }
 }
