9899a88dd871f2c24a0af755c2d2ef9df827c604
Tomas Bosak <bosak.tomas@gmail.com>
2013-04-15 13:29:34 +0200

    Add nuget package file.

---
+++ src/Orient/Orient.Tests/Pool/ConnectionPoolTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orient.Tests.Pool
+{
+    [TestClass]
+    public class ConnectionPoolTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Orient/Orient.Tests/Pool/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orient.Tests.Pool
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
