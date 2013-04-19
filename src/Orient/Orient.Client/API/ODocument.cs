e7866e86dca35cb2524d9fb63fd1b5ae3e031228
Tomas Bosak <bosak.tomas@gmail.com>
2013-04-19 11:16:46 +0200

    Move parsing and mapping to ODocument.

--- src/Orient/Orient.Client/API/ODocument.cs
+++ src/Orient/Orient.Client/API/ODocument.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace Orient.Client
@@ -44,7 +45,7 @@ namespace Orient.Client
                                 if (enumerator.Current is ODocument)
                                 {
                                     var instance = Activator.CreateInstance(elementType);
-                                    ((ODocument)enumerator.Current).MapData(ref instance);
+                                    ((ODocument)enumerator.Current).Map(ref instance);
 
                                     ((IList)value).Add(instance);
                                 }
@@ -89,7 +90,7 @@ namespace Orient.Client
                             if (enumerator.Current is ODocument)
                             {
                                 var instance = Activator.CreateInstance(elementType);
-                                ((ODocument)enumerator.Current).MapData(ref instance);
+                                ((ODocument)enumerator.Current).Map(ref instance);
 
                                 ((IList)value).Add(instance);
                             }
@@ -199,26 +200,13 @@ namespace Orient.Client
             return contains;
         }
 
-        public void MapData(ref object obj)
+        public T To<T>() where T : class, new()
         {
-            if (obj is Dictionary<string, object>)
-            {
-                obj = this;
-            }
-            else
-            {
-                Type objType = obj.GetType();
+            T genericObject = new T();
 
-                foreach (KeyValuePair<string, object> item in this)
-                {
-                    PropertyInfo property = objType.GetProperty(item.Key);
+            genericObject = (T)ToObject<T>(genericObject, "");
 
-                    if (property != null)
-                    {
-                        property.SetValue(obj, item.Value, null);
-                    }
-                }
-            }
+            return genericObject;
         }
 
         public static ODocument ToDocument<T>(T genericObject)
@@ -246,6 +234,124 @@ namespace Orient.Client
             }
 
             return document;
+        }
+
+        private T ToObject<T>(T genericObject, string path) where T : class, new()
+        {
+            Type genericObjectType = genericObject.GetType();
+
+            foreach (PropertyInfo propertyInfo in genericObjectType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                string propertyName = propertyInfo.Name;
+                OProperty oProperty = propertyInfo.GetCustomAttribute<OProperty>();
+
+                if (oProperty != null)
+                {
+                    propertyName = oProperty.Alias;
+                }
+
+                string fieldPath = path + (path != "" ? "." : "") + propertyName;
+
+                if ((propertyInfo.PropertyType.IsArray || propertyInfo.PropertyType.IsGenericType))
+                {
+                    if (this.HasField(fieldPath))
+                    {
+                        object propertyValue = this.GetField<object>(fieldPath);
+
+                        IList collection = (IList)propertyValue;
+
+                        if (collection.Count > 0)
+                        {
+                            // create instance of property type
+                            object collectionInstance = Activator.CreateInstance(propertyInfo.PropertyType, collection.Count);
+
+                            for (int i = 0; i < collection.Count; i++)
+                            {
+                                // collection is simple array
+                                if (propertyInfo.PropertyType.IsArray)
+                                {
+                                    ((object[])collectionInstance)[i] = collection[i];
+                                }
+                                // collection is generic
+                                else if (propertyInfo.PropertyType.IsGenericType && (propertyValue is IEnumerable))
+                                {
+                                    Type elementType = collection[i].GetType();
+
+                                    // generic collection consists of basic types or ORIDs
+                                    if (elementType.IsPrimitive ||
+                                        (elementType == typeof(string)) ||
+                                        (elementType == typeof(DateTime)) ||
+                                        (elementType == typeof(decimal)) ||
+                                        (elementType == typeof(ORID)))
+                                    {
+                                        ((IList)collectionInstance).Add(collection[i]);
+                                    }
+                                    // generic collection consists of generic type which should be parsed
+                                    else
+                                    {
+                                        // create instance object based on first element of generic collection
+                                        object instance = Activator.CreateInstance(propertyInfo.PropertyType.GetGenericArguments().First(), null);
+
+                                        ((IList)collectionInstance).Add(ToObject(instance, fieldPath));
+                                    }
+                                }
+                                else
+                                {
+                                    object v = Activator.CreateInstance(collection[i].GetType(), collection[i]);
+
+                                    ((IList)collectionInstance).Add(v);
+                                }
+                            }
+
+                            propertyInfo.SetValue(genericObject, collectionInstance, null);
+                        }
+                    }
+                }
+                // property is class except the string or ORID type since string and ORID values are parsed differently
+                else if (propertyInfo.PropertyType.IsClass &&
+                    (propertyInfo.PropertyType.Name != "String") &&
+                    (propertyInfo.PropertyType.Name != "ORID"))
+                {
+                    // create object instance of embedded class
+                    object instance = Activator.CreateInstance(propertyInfo.PropertyType);
+
+                    propertyInfo.SetValue(genericObject, ToObject(instance, fieldPath), null);
+                }
+                // property is basic type
+                else
+                {
+                    if (this.HasField(fieldPath))
+                    {
+                        object propertyValue = this.GetField<object>(fieldPath);
+
+                        propertyInfo.SetValue(genericObject, propertyValue, null);
+                    }
+                }
+            }
+
+            return genericObject;
+        }
+
+        private void Map(ref object obj)
+        {
+            if (obj is Dictionary<string, object>)
+            {
+                obj = this;
+            }
+            else
+            {
+                Type objType = obj.GetType();
+
+                foreach (KeyValuePair<string, object> item in this)
+                {
+                    PropertyInfo property = objType.GetProperty(item.Key);
+
+                    if (property != null)
+                    {
+                        property.SetValue(obj, item.Value, null);
+                    }
+                }
+            }
         }
     }
 }
