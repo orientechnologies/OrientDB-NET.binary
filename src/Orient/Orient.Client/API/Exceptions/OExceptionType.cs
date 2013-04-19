8ff432bce09a41df39fdf110dad8f8381228f0df
Tomas Bosak <bosak.tomas@gmail.com>
2013-04-19 15:21:38 +0200

    Add document type exception.

--- src/Orient/Orient.Client/API/Exceptions/OExceptionType.cs
+++ src/Orient/Orient.Client/API/Exceptions/OExceptionType.cs
@@ -4,9 +4,10 @@ namespace Orient.Client
     public enum OExceptionType
     {
         Connection,
+        Deserialization,
+        Document,
         Operation,
         Query,
-        Serialization,
-        Deserialization
+        Serialization
     }
 }
