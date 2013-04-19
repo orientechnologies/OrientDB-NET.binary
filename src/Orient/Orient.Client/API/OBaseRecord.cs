ef774f82cf386a1f36db552ea2c2d910356f5a8c
Tomas Bosak <bosak.tomas@gmail.com>
2013-04-19 10:22:38 +0200

    Create OBaseRecord abstract class with common fields for model classes.

--- src/Orient/Orient.Client/API/OBaseRecord.cs
+++ src/Orient/Orient.Client/API/OBaseRecord.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Orient.Client;
 
-namespace Orient.Client.API
+namespace Orient.Client
 {
-    class OBaseRecord
+    public abstract class OBaseRecord
     {
+        [OProperty(Serializable = false, Deserializable = false)]
+        public ORID ORID { get; set; }
+
+        [OProperty(Serializable = false, Deserializable = false)]
+        public int OVersion { get; set; }
+
+        [OProperty(Serializable = false, Deserializable = false)]
+        public short OClassId { get; set; }
+
+        [OProperty(Serializable = false, Deserializable = false)]
+        public string OClassName { get; set; }
+
+        public ODocument ToDocument()
+        {
+            return ODocument.ToDocument(this);
+        }
     }
 }
