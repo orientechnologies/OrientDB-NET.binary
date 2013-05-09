0482025cda25f4ff7a08085a7b28a6728633328b
Tomas Bosak <bosak.tomas@gmail.com>
2013-05-09 11:36:12 +0200

    Bump version number and nuget pakcage.

--- src/Orient/Orient.Client/API/OClient.cs
+++ src/Orient/Orient.Client/API/OClient.cs
@@ -10,7 +10,7 @@ namespace Orient.Client
         internal static string ClientID { get { return "null"; } }
 
         public static string DriverName { get { return "OrientDB-NET.binary"; } }
-        public static string DriverVersion { get { return "0.2.0"; } }
+        public static string DriverVersion { get { return "0.2.1"; } }
         public static short ProtocolVersion { get { return 15; } }
         public static int BufferLenght { get; set; }
 
