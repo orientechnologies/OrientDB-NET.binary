f950bb52d3290d0e4626645ed7570bc8b5da4cc4
Tomas Bosak <bosak.tomas@gmail.com>
2013-05-11 12:02:24 +0200

    Move OBaseRecord to Types folder.

---
+++ src/Orient/Orient.Tests/Graph/GraphClassesTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orient.Tests.Graph
+{
+    [TestClass]
+    public class GraphClassesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
