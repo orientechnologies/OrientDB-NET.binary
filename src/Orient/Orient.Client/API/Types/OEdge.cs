d0c727348aeeed34db7addd07bace7b08b8e422c
Tomas Bosak <bosak.tomas@gmail.com>
2013-05-11 12:05:13 +0200

    Refactor OEdge to use ODocument fields.

--- src/Orient/Orient.Client/API/Types/OEdge.cs
+++ src/Orient/Orient.Client/API/Types/OEdge.cs
@@ -1,12 +1,44 @@
-﻿
+﻿using System;
+using System.Reflection;
+
 namespace Orient.Client
 {
-    public class OEdge : OBaseRecord
+    public class OEdge : ODocument
     {
-        [OProperty(Alias = "in")]
-        public ORID In { get; set; }
+        [OProperty(Alias = "in", Serializable = false)]
+        public ORID InV
+        {
+            get
+            {
+                return this.GetField<ORID>("in");
+            }
+        }
 
-        [OProperty(Alias = "out")]
-        public ORID Out { get; set; }
+        [OProperty(Alias = "out", Serializable = false)]
+        public ORID OutV
+        {
+            get
+            {
+                return this.GetField<ORID>("out");
+            }
+        }
+
+        [OProperty(Alias = "label", Serializable = false)]
+        public string Label 
+        {
+            get
+            {
+                string label = this.GetField<string>("@OClassName");
+
+                if (string.IsNullOrEmpty(label))
+                {
+                    return this.GetType().Name;
+                }
+                else
+                {
+                    return label;
+                }
+            }
+        }
     }
 }
