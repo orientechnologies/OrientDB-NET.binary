94eace30f89cdf98b9a3e2a4027083a0f1ae0383
Robin Glover <robin.glover@workshare.com>
2014-10-16 10:45:17 +0100

    Improve support for automatic property creation when creating schema classes

---
+++ src/Orient/Orient.Client/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Orient.Client
+{
+    static class ExtensionMethods
+    {
+        public static OProperty GetOPropertyAttribute(this PropertyInfo property)
+        {
+            return property.GetCustomAttributes(typeof(OProperty), true).OfType<OProperty>().FirstOrDefault();
+        }
+    }
+}
