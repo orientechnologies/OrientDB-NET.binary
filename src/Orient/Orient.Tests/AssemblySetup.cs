43d1c65fa3c20499181f6204ca81a7bba6e9db47
Robin <robin.glover@workshare.com>
2014-10-17 10:02:44 +0100

    File naming tidy up, update README.md

---
+++ src/Orient/Orient.Tests/AssemblySetup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orient.Tests
+{
+    [TestClass]
+    public class AssemblySetup
+    {
+        [AssemblyInitialize()]
+        public static void Setup(TestContext context)
+        {
+            // orientDbDir needs to point to the path of an OrientDB installation (pointing to the folder that contains the bin, lib, config sub folders)
+            var orientDBDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\..\..\orient.server"));
+            // jreDir needs to point to a JRE (or JDK)
+            var jreDir = @"C:\Program Files\Java\jre7";
+            DbRunner.StartOrientDb(orientDBDir, jreDir);
+        }
+
+        [AssemblyCleanup()]
+        public static void TearDown()
+        {
+            DbRunner.StopOrientDb();
+        }
+    }
+}
