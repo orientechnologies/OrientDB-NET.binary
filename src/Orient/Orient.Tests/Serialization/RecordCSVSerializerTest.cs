5110234db80b247135c53aae69cb3a6acb0f092c
unknown <robert.taylor@BGL-RTAYLOR2.Betgenius.local>
2015-01-09 19:20:27 +0000

    The RecordCSVSerializer doesn't deserialize record strings containing embedded documents correctly. I've made minor bug fixes to get this working.

---
+++ src/Orient/Orient.Tests/Serialization/RecordCSVSerializerTest.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orient.Client;
+using Orient.Client.Protocol.Serializers;
+
+namespace Orient.Tests.Serialization
+{
+    [TestClass]
+    public class RecordCSVSerializerTest
+    {
+        [TestMethod]
+        public void CanDeserializeARecordStringContainingNestedEmbeddedDocuments()
+        {
+            // Arrange
+            var recordCsvSerializer = new RecordCSVSerializer(null);
+            const int innerInteger = 1;
+
+            string recordString = "TestClassName@EmbeddedDocument:{\"NestedEmbeddedDocument\":{\"InnerInteger\":"+innerInteger+"}}";
+
+            // Act
+            var actualDocument = recordCsvSerializer.Deserialize(recordString);
+
+            // Assert
+            Assert.AreEqual(innerInteger, actualDocument.GetField<int>("EmbeddedDocument.NestedEmbeddedDocument.InnerInteger"));
+        }
+
+        [TestMethod]
+        public void CanDeserializeARecordStringContainingNestedArraysOfEmbeddedDocuments()
+        {
+            // Arrange
+            var recordCsvSerializer = new RecordCSVSerializer(null);
+            const int innerInteger = 1;
+
+            string recordString = "TestClassName@EmbeddedDocument:[{\"NestedEmbeddedDocument\":[{\"InnerInteger\":" + innerInteger + "}]}]";
+
+            // Act
+            var actualDocument = recordCsvSerializer.Deserialize(recordString);
+
+            // Assert
+            var firstLevelEmbeddedDocuments = actualDocument.GetField<List<object>>("EmbeddedDocument").Cast<ODocument>().ToArray();
+            var nestedDocuments = firstLevelEmbeddedDocuments[0].GetField<List<object>>("NestedEmbeddedDocument").Cast<ODocument>().ToArray();
+
+            Assert.AreEqual(1, firstLevelEmbeddedDocuments.Length);
+            Assert.AreEqual(1, nestedDocuments.Length);
+            Assert.AreEqual(innerInteger, nestedDocuments[0].GetField<int>("InnerInteger"));
+        }
+    }
+}
