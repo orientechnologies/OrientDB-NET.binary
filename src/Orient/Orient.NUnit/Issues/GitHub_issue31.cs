2de8f4ac076becad8dfc3f414c504909c1cedf2b
Shawn Poindexter <shawn@poindexter.us>
2015-02-09 12:45:57 -0800

    test case for issue #31
    this test should pass

---
+++ src/Orient/Orient.NUnit/Issues/GitHub_issue31.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using Orient.Client;
+
+namespace Orient.Tests.Issues
+{
+    [TestFixture]
+    public class GitHub_issue31
+    {
+        [Test]
+        public void ProblemInsertingObjectWithDateTimeFields_31()
+        {
+            using (TestDatabaseContext testContext = new TestDatabaseContext())
+            using (ODatabase database = new ODatabase(TestConnection.GlobalTestDatabaseAlias))
+            {
+                var startRecords = database.CountRecords;
+
+                database.Create.Class<File>().Extends<OVertex>().CreateProperties().Run();
+                database.Insert(new File
+                    {
+                        Filename = "myfile",
+                        Created = DateTime.Now
+                    }).Run();
+
+                var endRecords = database.CountRecords;
+                Assert.AreEqual(startRecords + 1, endRecords);
+            }
+
+        }
+
+        class File
+        {
+            public String Filename { get; set; }
+
+            public DateTime Created { get; set; }
+        }
+    }
+}
+
