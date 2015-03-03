aae1af2dcabeb414cf69f29dbce4e6833f992133
Robin <robin.glover@workshare.com>
2015-03-03 11:22:50 +0000

    Startup and shutdown orientdb for the running of the tests, but only if it is not already listening on port 2424

--- src/Orient/Orient.NUnit/AssemblySetup.cs
+++ src/Orient/Orient.NUnit/AssemblySetup.cs
@@ -7,11 +7,13 @@ namespace Orient.Tests
     [SetUpFixture]
     public class AssemblySetup
     {
+        private static string _orientServerDirectory = @"..\..\..\..\..\..\orient.server";
+
         [SetUp]
         public static void Setup()
         {
             // orientDbDir needs to point to the path of an OrientDB installation (pointing to the folder that contains the bin, lib, config sub folders)
-            var orientDBDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\..\..\..\orient.server"));
+            var orientDBDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, _orientServerDirectory));
             // jreDir needs to point to a JRE (or JDK)
             var jreDir = @"C:\Program Files\Java\jre7";
             DbRunner.StartOrientDb(orientDBDir, jreDir);
@@ -20,7 +22,7 @@ namespace Orient.Tests
         [TearDown]
         public static void TearDown()
         {
-            DbRunner.StopOrientDb();
+            DbRunner.StopOrientDb(_orientServerDirectory);
         }
     }
 }
--- src/Orient/Orient.NUnit/DbRunner.cs
+++ src/Orient/Orient.NUnit/DbRunner.cs
@@ -2,18 +2,127 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Management;
+using System.Net.NetworkInformation;
 using System.Threading;
 
 namespace Orient.Tests
 {
     public class DbRunner
     {
+        private static Process _process;
+
+
         public static void StartOrientDb(string dbDir, string javaDir)
         {
+            if (IsOrientAlreadyListening())
+                return;
+
+            _process = new Process();
+            var path = Path.Combine(dbDir, @"bin\server.bat");
+
+            _process.StartInfo.FileName = path;
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.EnvironmentVariables.Add("ORIENTDB_HOME", dbDir);
+            _process.StartInfo.EnvironmentVariables.Add("JAVA_HOME", javaDir);
+            _process.StartInfo.WorkingDirectory = dbDir;
+            //_process.StartInfo.RedirectStandardError = true;
+            //_process.StartInfo.RedirectStandardOutput = true;
+            _process.Start();
+
+
+            Thread.Sleep(1000);
+
+            //var output = _process.StandardOutput.ReadToEnd();
+            //var error = _process.StandardError.ReadToEnd();
+
+            if (_process.HasExited)
+                throw new Exception("OrientDB did not start correctly");
+
         }
 
-        public static void StopOrientDb()
+        private static bool IsOrientAlreadyListening()
         {
+            const int orientPort = 2424;
+
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+
+            foreach (var tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.Port == orientPort)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public static void StopOrientDb(string dbDir)
+        {
+            if (_process == null)
+                return;
+
+            var path = Path.Combine(dbDir, @"bin\shutdown.bat");
+            Process.Start(path);
+            Thread.Sleep(1000);
+
+            ProcessUtilities.KillProcessTree(_process);
+        }
+    }
+
+    class ProcessUtilities
+    {
+        public static void KillProcessTree(Process root)
+        {
+            if (root != null)
+            {
+                var list = new List<Process>();
+                GetProcessAndChildren(Process.GetProcesses(), root, list, 1);
+
+                foreach (Process p in list)
+                {
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch (Exception)
+                    {
+                        //Log error?
+                    }
+                }
+            }
+        }
+
+        private static int GetParentProcessId(Process p)
+        {
+            int parentId = 0;
+            try
+            {
+                ManagementObject mo = new ManagementObject("win32_process.handle='" + p.Id + "'");
+                mo.Get();
+                parentId = Convert.ToInt32(mo["ParentProcessId"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                parentId = 0;
+            }
+            return parentId;
+        }
+
+        private static void GetProcessAndChildren(Process[] plist, Process parent, List<Process> output, int indent)
+        {
+            foreach (Process p in plist)
+            {
+                if (GetParentProcessId(p) == parent.Id)
+                {
+                    GetProcessAndChildren(plist, p, output, indent + 1);
+                }
+            }
+            output.Add(parent);
         }
     }
 }
