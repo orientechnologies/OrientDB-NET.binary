11b2b83f2aac80f72edd7dc97354bf84bcb3779a
sh.goh@iwowsoft.com <sh.goh@iwowsoft.com>
2015-03-06 18:35:14 +0800

    Fix bug for issue #48 long and short type not supported

---
+++ src/Orient/Orient.Client/Mapping/ShortFieldMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Orient.Client.Mapping
+{
+    internal class ShortFieldMapping<TTarget> : BasicNamedFieldMapping<TTarget>
+    {
+        public ShortFieldMapping(PropertyInfo propertyInfo, string fieldPath)
+            : base(propertyInfo, fieldPath)
+        {
+
+        }
+        protected override void MapToNamedField(ODocument document, TTarget typedObject)
+        {
+            // Only until bug #3483 will be fixed than use decimal
+            object item = document.GetField<object>(_fieldPath);
+            if (item is IConvertible)
+                SetPropertyValue(typedObject, Convert.ChangeType(item, typeof(short)));
+        }
+    }
+}
