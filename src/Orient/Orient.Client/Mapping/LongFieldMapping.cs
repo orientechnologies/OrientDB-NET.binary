26f8e76375808d576095403e027344df51537b14
Roman Gordon <goormoon@gmail.com>
2015-03-06 14:09:32 +0200

    Fix issue after merge brunch

--- src/Orient/Orient.Client/Mapping/LongFieldMapping.cs
+++ src/Orient/Orient.Client/Mapping/LongFieldMapping.cs
@@ -13,15 +13,8 @@ namespace Orient.Client.Mapping
         {
 
         }
-<<<<<<< HEAD
-
         protected override void MapToNamedField(ODocument document, TTarget typedObject)
         {
-=======
-        protected override void MapToNamedField(ODocument document, TTarget typedObject)
-        {
-            // Only until bug #3483 will be fixed than use decimal
->>>>>>> 49f1bc93f51639abe0c0337a2fea45d8adfcc513
             object item = document.GetField<object>(_fieldPath);
             if (item is IConvertible)
                 SetPropertyValue(typedObject, Convert.ChangeType(item, typeof(long)));
