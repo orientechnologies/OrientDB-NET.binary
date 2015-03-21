7da00cba52826c59e72fdda16504a31e0bfb31c0
Roman Gordon <goormoon@gmail.com>
2015-03-21 17:04:21 +0200

    Issue #62 Dictionary with single entry

---
+++ src/Orient/Orient.NUnit/Issues/GitHub_issue62.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Orient.Client;
+
+namespace Orient.Tests.Issues
+{
+    [TestFixture(Category = "issue")]
+    public class GitHub_issue62
+    {
+        TestDatabaseContext _context;
+        ODatabase _database;
+
+        [SetUp]
+        public void Init()
+        {
+            _context = new TestDatabaseContext();
+            _database = new ODatabase(TestConnection.GlobalTestDatabaseAlias);
+
+            _database.Create.Class<Person>().Run();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _database.Dispose();
+            _context.Dispose();
+        }
+
+        [Test]
+        public void ShouldInsertRetrieveSingleDictionaryProperty()
+        {
+            var person = new Person
+            {
+                Name = "Luca",
+                EmbededMap = new Dictionary<string, int>()
+            };
+            person.EmbededMap.Add("test", 100);
+
+            var document = _database.Insert<Person>(person).Run();
+            var createdPerson = document.To<Person>();
+            Assert.That(createdPerson.EmbededMap["test"], Is.EqualTo(100));
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public Dictionary<string, int> EmbededMap { get; set; }
+        }
+    }
+}
