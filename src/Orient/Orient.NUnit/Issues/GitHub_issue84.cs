ca70f562e69c140985bbd643eb1aae4de49b35f5
Stefano Castriotta <stefanocastriotta@users.noreply.github.com>
2015-09-12 23:24:33 +0200

    Trailing backslash issue #84

--- src/Orient/Orient.NUnit/Issues/GitHub_issue84.cs
+++ src/Orient/Orient.NUnit/Issues/GitHub_issue84.cs
@@ -50,5 +50,44 @@ namespace Orient.Tests.Issues
             Assert.AreEqual(insertedDocument.GetField<string>("foo"), document.GetField<string>("foo"));
             Assert.AreEqual(insertedDocument.GetField<int>("bar"), document.GetField<int>("bar"));
         }
+
+        [Test]
+        public void ShouldInsertAndReadValuesWithTrailingBackslash()
+        {
+            _database
+                         .Create.Class("TestClass")
+                         .Run();
+
+            ODocument document = new ODocument();
+            document.OClassName = "TestClass";
+            document
+                .SetField("foo", "foo string value \\ test \\")
+                .SetField("bar", 12345);
+
+            ODocument insertedDocument = _database
+                .Insert(document)
+                .Run();
+
+            Assert.IsTrue(insertedDocument.ORID != null);
+            Assert.AreEqual(insertedDocument.OClassName, "TestClass");
+            Assert.AreEqual(insertedDocument.GetField<string>("foo"), document.GetField<string>("foo"));
+            Assert.AreEqual(insertedDocument.GetField<int>("bar"), document.GetField<int>("bar"));
+
+
+            List<ODocument> documents = _database
+                        .Select()
+                        .From(insertedDocument)
+                        .ToList();
+
+            Assert.AreEqual(documents.Count, 1);
+
+            for (int i = 0; i < documents.Count; i++)
+            {
+                Assert.AreEqual(documents[i].ORID, insertedDocument.ORID);
+                Assert.AreEqual(documents[i].OClassName, insertedDocument.OClassName);
+                Assert.AreEqual(documents[i].GetField<string>("foo"), insertedDocument.GetField<string>("foo"));
+                Assert.AreEqual(documents[i].GetField<int>("bar"), insertedDocument.GetField<int>("bar"));
+            }
+        }
     }
 }
